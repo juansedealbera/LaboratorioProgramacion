4006eec58faf30db486d817b05df26a6814f1702
juansedealbera <dealberaj@gmail.com>
2025-03-31 11:57:37 -0300

    Agregar archivos de proyecto.

---
+++ LaboratorioProgramacion3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgramacion3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Buen lunes para programar!");
+        }
+    }
+}
